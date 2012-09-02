dd004a83eb659624254c68634a03611a6f374e8a
ruiiiijiiiiang <me@ruijiang.me>
2012-09-01 19:58:46 -0500

    worked on editor

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -5,7 +5,16 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    config.toolbar = 'MyToolbar';
+
+    config.toolbar_MyToolbar =
+    [
+        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','-','Undo','Redo' ] },
+        { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker','Scayt' ] },
+        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent' ] },
+        '/',
+        { name: 'styles', items : [ 'Styles','Format' ] },
+        { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','-','RemoveFormat' ] },
+        { name: 'colors', items : [ 'TextColor','BGColor' ] },
+    ];
 };
