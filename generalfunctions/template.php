edb7f14459bceeb45b372284682ced2e67fbf3d7
ruiiiijiiiiang <me@ruijiang.me>
2012-09-15 17:00:43 -0500

    fixed git commit information

--- generalfunctions/template.php
+++ generalfunctions/template.php
@@ -1,57 +1,52 @@
 
 <?php
-<<<<<<< HEAD
-if (!isset($_SESSION))
-{
-=======
 if (!isset ($_COOKIE[ini_get('session.name')])) {
->>>>>>> 6667d6d3d53f91727b8098bb8760bc58b897f9fd
-	session_start();
+    session_start();
 }
 /**
 Creates the page for html.
 **/
 function printFooter()
 {
-	return("<div align='center' id='footer'>Professional Archives&copy; <a href='mailto: contact@proarcs.com'>Contact Us</a></div>");
+    return("<div align='center' id='footer'>Professional Archives&copy; <a href='mailto: contact@proarcs.com'>Contact Us</a></div>");
 }
 /**
 Creates the navigation bar.
 **/
 function navBar($mes)
 {
-	//echo $mes;
-	if ( isset($SESSION['business_mode'])&&$_SESSION['business_mode'])
-	{
-		$message = "<div id='main-menu'>
-				<ul class='sf-menu fright responsive-menu'>
-					<li class='current'><a href='http://www.proarcs.com/home.php'>home</a></li>
-					<li><a href='http://www.proarcs.com/profile.php'>profile<span class='arrow'></span></a><ul>
-							<li><a href='http://www.proarcs.com/business.php'>Edit Business Information</a></li>
-							<li><a href='http://www.proarcs.com/supplemental.php'>Add Supplemental Forms</a></li>
-						</ul>
-					</li>
-					<li><a href='http://www.proarcs.com/inbox.php'>inbox";
-		if ($mes > 0)
-		{
-			$message = $message."(".$mes.")";
-		}
-		$message .= "</a></li><li><a href='http://www.proarcs.com/careers.php'>careers</a></li>
-					 <li><a href='#'>&darr;</a><span class='arrow'></span></a><ul>
-							<li><a href='http://www.proarcs.com/privacysettings.php'>Privacy Settings</a></li>
-							<li><a href='mailto: contact@proarcs.com'>Contact Us</a></li>
-							<li><a href='http://www.proarcs.com/logout.php'>Log Out</a></li>
-							</ul>
-				</ul>
-			</div>";
-	}
-	else
-	{
-		$message = "<div id='main-menu'>
-				<ul class='sf-menu fright responsive-menu'>
-					<li class='current'><a href='http://www.proarcs.com/home.php'>home</a></li>
-					<li><a href='http://www.proarcs.com/cprofile.php'>profile<span class='arrow'></span></a><ul>
-							<li><a href='http://www.proarcs.com/basic_info.php'>Edit Basic Information</a></li>
+    //echo $mes;
+    if ( isset($SESSION['business_mode'])&&$_SESSION['business_mode'])
+    {
+        $message = "<div id='main-menu'>
+            <ul class='sf-menu fright responsive-menu'>
+            <li class='current'><a href='http://www.proarcs.com/home.php'>home</a></li>
+            <li><a href='http://www.proarcs.com/profile.php'>profile<span class='arrow'></span></a><ul>
+            <li><a href='http://www.proarcs.com/business.php'>Edit Business Information</a></li>
+            <li><a href='http://www.proarcs.com/supplemental.php'>Add Supplemental Forms</a></li>
+            </ul>
+            </li>
+            <li><a href='http://www.proarcs.com/inbox.php'>inbox";
+        if ($mes > 0)
+        {
+            $message = $message."(".$mes.")";
+        }
+        $message .= "</a></li><li><a href='http://www.proarcs.com/careers.php'>careers</a></li>
+            <li><a href='#'>&darr;</a><span class='arrow'></span></a><ul>
+            <li><a href='http://www.proarcs.com/privacysettings.php'>Privacy Settings</a></li>
+            <li><a href='mailto: contact@proarcs.com'>Contact Us</a></li>
+            <li><a href='http://www.proarcs.com/logout.php'>Log Out</a></li>
+            </ul>
+            </ul>
+            </div>";
+    }
+    else
+    {
+        $message = "<div id='main-menu'>
+            <ul class='sf-menu fright responsive-menu'>
+            <li class='current'><a href='http://www.proarcs.com/home.php'>home</a></li>
+            <li><a href='http://www.proarcs.com/cprofile.php'>profile<span class='arrow'></span></a><ul>
+            <li><a href='http://www.proarcs.com/basic_info.php'>Edit Basic Information</a></li>
 							<li><a href='http://www.proarcs.com/education.php'>Edit Education</a></li>
 							<li><a href='http://www.proarcs.com/work.php'>Add Work Experience</a></li>
 							<li><a href='http://www.proarcs.com/extracurricular.php'>Add Extracurriculars</a></li>
